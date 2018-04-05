aa5cdde4a76e70594d194e8b4b4f7e684e56ac00
e067411 <pramod.jangidi@mastercard.com>
2018-04-05 14:21:27 -0400

    added projects

---
+++ weather-app/src/test/java/com/pramod/cloud/weatherapp/WheatherAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pramod.cloud.weatherapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WheatherAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
